3ddaca6d70382f3fbd6303ae17b118af6a32aea7
Taisiia Karasova <karasova.t@phystech.edu>
2021-04-26 22:54:17 +0300

    Added lab10

---
+++ lab10/Bag/B.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int costs[101];
+int masses[101];
+int table_calc[1001][101];
+
+int main()
+{
+    int w;
+    int n;
+
+
+    cin >> w >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> costs[i];
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> masses[i];
+    }
+
+    for (int i = 0; i <= n; i++)
+    {
+        table_calc[0][i] = 0;
+    }
+
+    for (int i = 0; i <= w; i++)
+    {
+        table_calc[i][0] = 0;
+    }
+
+    for (int j = 1; j <= n; j++)
+    {
+        for (int i = 1; i <= w; i++)
+        {
+            if (i >= masses[j])
+            {
+                table_calc[i][j] = max(table_calc[i - masses[j]][j - 1] + costs[j], table_calc[i][j - 1]);
+            }
+            else
+            {
+                table_calc[i][j] = table_calc[i][j - 1];
+            }
+        }
+    }
+
+    cout << table_calc[w][n] << '\n';
+
+
+    return 0;
+}
+
---
+++ lab10/Exchange/A.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int monets[101];
+int table_calc[1001][101];
+
+int main()
+{
+    int s;
+    int n;
+    cin >> s >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> monets[i];
+    }
+    for (int i = 0; i <= n; i++)
+    {
+        table_calc[0][i] = 0;
+    }
+    for (int i = 1; i <= s; i++)
+    {
+        table_calc[i][0] = s + 1;
+    }
+
+
+    sort(monets, monets + n);
+
+    for (int j = 1; j <= n; j++)
+    {
+        for (int i = 1; i <= s; i++)
+        {
+            if (monets[j] > i)
+                table_calc[i][j] = table_calc[i][j - 1];
+            else
+                table_calc[i][j] = min(table_calc[i][j - 1], table_calc[i - monets[j]][j] + 1);
+        }
+    }
+
+   
+    cout << table_calc[s][n] << '\n';
+
+
+
+    return 0;
+}
+
+
